c25bbcea8c84c8866248044e53ef0c59e1ae5677
barresh <giuseppebarreca@yahoo.com>
2023-08-22 11:08:04 +0200

    [FE] Fatte tante cose, come nuovo branch

--- frontend/src/app/tab4/tab4.page.ts
+++ frontend/src/app/tab4/tab4.page.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { WebSocketService } from '../websocket-service';
+
+
+
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss']
 })
+
 export class Tab4Component {
+
   constructor(private webSocketService:WebSocketService){}
   pin: string = "";
   pinDisplayValue: string = "";
   porcone:number =10;
   area1=1;
+  generatedPin = '111111';
+
+
   ngOnInit(){{
     this.webSocketService.getMessages().subscribe((message) => {
       console.log("messaggio ricevuto",message)
@@ -43,11 +51,15 @@ accendiArea1(){
 
 }
   sendPin() {
-    // Qui puoi gestire l'invio del PIN, ad esempio, chiamare un servizio o eseguire una logica specifica
-    console.log('PIN inviato:', this.pin);
-    // Resetta il PIN dopo l'invio
+    if (this.pin === this.generatedPin) {
+      console.log('PIN corretto!');
+      this.webSocketService.setAuthenticated(true); // Imposta lo stato di autenticazione a true
+    } else {
+      console.log('PIN errato!');
+    }
     this.resetPin();
   }
+  
 
   resetPin() {
     this.pin = "";
@@ -57,4 +69,7 @@ accendiArea1(){
   updatePinDisplay() {
     this.pinDisplayValue = "*".repeat(this.pin.length);
   }
+
+  
 }
+
