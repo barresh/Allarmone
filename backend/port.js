acd1ce570511b4948d66dc589c04f6c04a0b9cf4
tusiquespacchi <trono.gabriele@gmail.com>
2023-12-05 12:28:22 +0100

    a

--- backend/port.js
+++ backend/port.js
@@ -1,10 +1,7 @@
-// const { SerialPort } = require("serialport");
-// const port = new SerialPort({
-//   path: "/dev/ttyACM0",
-//   baudRate: 57600,
-// });
-const a6 = require("./A6");
-const port = require("bluetooth-serial");
+const { SerialPort } = require("serialport");
+const port = new SerialPort({
+  path: "/dev/ttyACM0",
+  baudRate: 57600,
+});
 
-const address = "00:80:E1:27:B3:CF"; // Sostituisci con l'indirizzo Bluetooth del dispositivo di destinazione
 exports.port = port;
