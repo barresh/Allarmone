ae81461ff378fcde7176fa3d25a1002cea5e8a0d
tusiquespacchi <trono.gabriele@gmail.com>
2023-12-05 10:17:36 +0100

    così

--- backend/port.js
+++ backend/port.js
@@ -3,38 +3,13 @@
 //   path: "/dev/ttyACM0",
 //   baudRate: 57600,
 // });
-
+const a6 = require("./A6");
 const { BluetoothSerialPort } = require("bluetooth-serial-port");
 const port = new BluetoothSerialPort();
 
 const address = "00:80:E1:27:B3:CF"; // Sostituisci con l'indirizzo Bluetooth del dispositivo di destinazione
-
-bluetoothSerial.connect(address, 1, function () {
-  console.log("Connesso con successo al dispositivo Bluetooth");
-
-  // Ora puoi utilizzare `bluetoothSerial` per la comunicazione Bluetooth
-
-  // Esempio di invio di dati
-  bluetoothSerial.write(
-    new Buffer("Hello, Bluetooth!"),
-    function (err, bytesWritten) {
-      if (err) console.log("Errore durante l'invio dei dati:", err);
-      else
-        console.log(
-          "Dati inviati correttamente. Numero di byte scritti:",
-          bytesWritten
-        );
-    }
-  );
-
-  // Esempio di ricezione di dati
-  bluetoothSerial.on("data", function (buffer) {
-    console.log("Dati ricevuti:", buffer.toString("utf-8"));
-  });
-});
-
-// Gestisci eventi di disconnessione
-bluetoothSerial.on("closed", function () {
-  console.log("Connessione Bluetooth chiusa");
-});
 exports.port = port;
+port.connect(address, 1, function () {
+  a6.entrata;
+  console.log("Connesso con successo al dispositivo Bluetooth");
+});
