2fc22bd603bf8c6aea8b6e7978944e16018139cb
tusiquespacchi <trono.gabriele@gmail.com>
2023-12-05 10:26:12 +0100

    a

--- backend/port.js
+++ backend/port.js
@@ -8,7 +8,3 @@ const port = require("bluetooth-serial");
 
 const address = "00:80:E1:27:B3:CF"; // Sostituisci con l'indirizzo Bluetooth del dispositivo di destinazione
 exports.port = port;
-port.connect(address, 1, function () {
-  a6.entrata;
-  console.log("Connesso con successo al dispositivo Bluetooth");
-});
